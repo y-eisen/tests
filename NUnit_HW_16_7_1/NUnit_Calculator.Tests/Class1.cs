c6631c2d88032491099aeb62279b96d1458e8752
y-eisen <eisenkrein@yandex.ru>
2022-11-17 22:09:06 +0300

    hw

---
+++ NUnit_HW_16_7_1/NUnit_Calculator.Tests/Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace NUnit_Calculator.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void AdditionalMustReturnTrue()
+        {
+            var calc = new Calculator();
+            Assert.True(calc.Additional(5, 12) == 17);                  
+        }
+        
+        [Test]
+        public void SubtractionMustReturnTrue()
+        {
+            var calc = new Calculator();
+            Assert.True(calc.Additional(21, 12) == 9);
+        }
+
+        [Test]
+        public void MiltiplicationMustReturnTrue()
+        {
+            var calc = new Calculator();
+            Assert.True(calc.Additional(5, 4) == 20);
+        }
+
+        [Test]
+        public void DivisionMustReturnTrue()
+        {
+            var calc = new Calculator();
+            Assert.True(calc.Additional(30, 6) == 5);
+        }
+
+        [Test]
+        public void DivisionMustReturnException()
+        {
+            var calc = new Calculator();
+            Assert.Throws<DivideByZeroException>(() => calc.Division(30, 0));
+        }
+    }
+}
---
+++ NUnit_HW_16_7_1/NUnit_Calculator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnit_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
